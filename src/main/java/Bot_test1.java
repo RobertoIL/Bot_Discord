4e39e7b66a270466ca56e72de7d8cfac5b573a44
roberto <robertillesca1@gmail.com>
2022-11-10 17:26:13 -0300

    hola

--- src/main/java/Bot_test1.java
+++ src/main/java/Bot_test1.java
@@ -7,13 +7,17 @@ import javax.security.auth.login.LoginException;
 public class Bot_test1 {
 
     public static void main(String[] args) throws LoginException {
-        final String token = "MTAzOTYzNDE5NzE2MzI4NjY0MA.GCXP9O.dSBG7JSym--Si_rFGlZ3nFGKrJPWjz0dsznF90";
+        final String token = "";
 
         JDABuilder jdaBuilder = JDABuilder.createDefault(token);
 
         jdaBuilder.build();
 
+        //hola
+
 
     }
+
+
 
 }
