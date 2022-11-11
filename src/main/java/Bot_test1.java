ed5c72340f28611b04d982f8587f83f165506920
roberto <robertillesca1@gmial.com>
2022-11-11 17:25:17 -0300

    ordenando las cositas

--- src/main/java/Bot_test1.java
+++ src/main/java/Bot_test1.java
@@ -1,19 +1,25 @@
 
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
 
 import javax.security.auth.login.LoginException;
 
 public class Bot_test1 {
+    private final Dotenv config;
 
-    public static void main(String[] args) throws LoginException {
-        final String token = "";
+
+    public Bot_test1(Dotenv config) throws LoginException{
+        this.config = config;
+
+        config = Dotenv.configure().load();
+        String token = config.get("TOKEN");
 
         JDABuilder jdaBuilder = JDABuilder.createDefault(token);
-
         jdaBuilder.build();
 
-        //hola
+
 
 
     }
