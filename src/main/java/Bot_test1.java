06b48947d3a2a2b5583f912d64413a965058d737
roberto <robertillesca1@gmail.com>
2022-11-17 17:41:39 -0300

    evento

--- src/main/java/Bot_test1.java
+++ src/main/java/Bot_test1.java
@@ -8,10 +8,6 @@ import javax.security.auth.login.LoginException;
 
 public class Bot_test1 {
     public static void main(String[] args) {
-        //final Dotenv config;
-
-        //config = Dotenv.configure().load();
-        //String token = config.get("TOKEN");
         String token = "";
         JDABuilder jdaBuilder = JDABuilder.createDefault(token);
         jdaBuilder.build();
