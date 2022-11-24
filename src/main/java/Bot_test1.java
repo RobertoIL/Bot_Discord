9a3a7ba8d3c17a4c8bcc3f421e1f5d30172780ff
roberto <robertillesca1@gmail.com>
2022-11-24 17:48:52 -0300

    .

--- src/main/java/Bot_test1.java
+++ src/main/java/Bot_test1.java
@@ -8,12 +8,16 @@ import net.dv8tion.jda.api.sharding.ShardManager;
 public class Bot_test1 {
     public static void main(String[] args) {
         ShardManager shardManager;
-        String token = "";
+        final String token = "MTAzOTYzNDE5NzE2MzI4NjY0MA.GIKPVo.Y54ItOcnXgT_Jb77vUJqK1BNVywGgv-qMns2SY";
 
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         builder.setStatus(OnlineStatus.ONLINE);
         shardManager = builder.build();
         shardManager.addEventListener(new EventListener());
+
+
+
+
 
 
     }
