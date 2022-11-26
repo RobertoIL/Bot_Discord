b25e9b2211addd3d9c2649ab082af1c4b1eea53d
roberto <robertillesca1@gmial.com>
2022-11-26 18:30:01 -0300

    slash commands

--- src/main/java/Bot_test1.java
+++ src/main/java/Bot_test1.java
@@ -1,25 +1,31 @@
+import commands.BotCommands;
 import listeners.EventoReaccion;
-import listeners.EventoSaludo;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+
+import javax.security.auth.login.LoginException;
 
 
 public class Bot_test1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LoginException, InterruptedException {
         final String token = "";
 
         JDA jda = JDABuilder.createDefault(token)
                 .addEventListeners(new EventoReaccion())
-                .addEventListeners(new EventoSaludo())
-                .build();
+                .addEventListeners(new BotCommands())
+                .build().awaitReady();
 
+        Guild guild = jda.getGuildById("1039635186071117865");
 
+        if (guild != null){
+            guild.upsertCommand("juegos-gratis", "Ver los juegos que destacados gratuitos").queue();
+            guild.upsertCommand("ofertas", "Ver las ultimas ofertas destacadas").queue();
+        }
 
-
-
+        }
 
 
     }
 
-}
+
--- src/main/java/commands/BotCommands.java
+++ src/main/java/commands/BotCommands.java
@@ -1,5 +1,6 @@
 package commands;
 
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -7,6 +8,12 @@ import org.jetbrains.annotations.NotNull;
 public class BotCommands extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if(event.getName().equals("juegos-gratis")){
+            event.reply("Aqui estaran los juegos gratuitos de epic").queue();
 
+        } else if (event.getName().equals("ofertas")) {
+            event.reply("Aqui se mostraran las mejores ofertas destacadas").queue();
+
+        }
     }
 }
